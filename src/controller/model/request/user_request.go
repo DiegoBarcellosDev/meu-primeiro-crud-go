943f9d754c4b9cd882493e550863d07e5a229ada
DiegoBarcellos <diegobarcellos.dev@gmail.com>
2025-02-18 17:05:40 -0300

    feat: adiciona validação aos campos de usuário

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Email    string `json:"email,omitempty"`
-	Password string `json:"password,omitempty"`
-	Name     string `json:"name,omitempty"`
-	Age      int8   `json:"age,omitempty"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6,containsany=!@$%&*."`
+	Name     string `json:"name" binding:"required,min=4,max=100"`
+	Age      int8   `json:"age" binding:"required,min=1,max=140"`
 }
